f2f307ece6ff6f86dba91d74922928d0baa8a643
Tao Ma <tao.ma.1984@gmail.com>
2015-11-19 10:05:40 +0800

    Generate test binary in ./tmp file.

--- analyze.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-d=src/rpc
-b=$d/rpc.test
-
-echo 'png > cpu.png' | go tool pprof -ignore=testing $b $d/cpu.out
-echo 'png > memo.png'| go tool pprof -ignore=testing -alloc_objects $b $d/mem.out
-echo 'png > mems.png'| go tool pprof -ignore=testing -alloc_space $b $d/mem.out
-echo 'png > block.png'| go tool pprof -ignore=testing $b $d/block.out
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+tmp='./tmp'
+
 . ./setup.sh
 
 # build msg
@@ -7,12 +9,31 @@ cd src/rpc
 ./gen.sh
 cd -
 
-go test rpc || exit 1
-cd src/rpc
-go test -c
-GODEBUG=gctrace=1
-GODEBUG=schedtrace=1000
-go test -bench=. -benchtime 10s  #-cpu 4 -cpuprofile cpu.out -memprofile mem.out -blockprofile block.out
-cd -
+if [ ! -e "$tmp" ]; then
+	mkdir "$tmp" || (echo "ERROR: cannot create tmp direcotry" >&2 && exit 1)
+fi
 
+pkgname='rpc'
 
+bin="$tmp/$pkgname.test"
+cpuprofile="$tmp/cpu.out"
+memprofile="$tmp/mem.out"
+blockprofile="$tmp/block.out"
+cpupng="$tmp/cpu.png"
+memspng="$tmp/mems.png"
+memopng="$tmp/memo.png"
+blockpng="$tmp/block.png"
+
+echo "INFO: build test binary file: $bin"
+go test -o "$bin" "$pkgname" || exit 1
+
+#GODEBUG=gctrace=1
+#GODEBUG=schedtrace=1000
+go test "$pkgname" -bench=. -benchtime 10s  #-cpu 4 -cpuprofile "$cpuprofile" -memprofile "$memprofile" -blockprofile "$blockprofile"
+
+if [ "$gen_analyze" = "true" ]; then
+	echo "png > $cpupng"  | go tool pprof -ignore=testing "$bin" "$cpuprofile"
+	echo "png > $memopng" | go tool pprof -ignore=testing -alloc_objects "$bin" "$memprofile"
+	echo "png > $memspng" | go tool pprof -ignore=testing -alloc_space "$bin" "$memprofile"
+	echo "png > $blockpng"| go tool pprof -ignore=testing "$bin" "$blockpng"
+fi
